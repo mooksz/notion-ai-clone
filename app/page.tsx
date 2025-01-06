9d21d0ff162cc952cbc740a1b552762dea1bfed2
Mikey Haklander <mikey.haklander@capgemini.com>
2025-01-06 13:31:35 +0100

    Update homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/atoms/Button/Button";
+import { ArrowLeftCircle } from "lucide-react";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Lets build</h1>
-
-      <Button>Click me</Button>
+    <main className="flex space-x-2 items-center animate-pulse">
+      <ArrowLeftCircle className="w-12 h-12" />
+      <h1 className="font-bold">Get started with creating a new document</h1>
     </main>
   );
 }
